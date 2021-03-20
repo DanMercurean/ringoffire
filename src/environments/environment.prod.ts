93ca7096a0be3bcbfa9250b1fd0fd5f5cea75829
Dan Mercurean <danymercurean@yahoo.com>
2021-03-20 13:21:33 +0100

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCEv1iZ80DWzGqRfKUx0wKuWZVRDSbHl7Y",
+    authDomain: "ring-of-fire-600e3.firebaseapp.com",
+    projectId: "ring-of-fire-600e3",
+    storageBucket: "ring-of-fire-600e3.appspot.com",
+    messagingSenderId: "675485532234",
+    appId: "1:675485532234:web:586784d8de1b81aa3366b3"
+  }
 };
